110ec17ab6a0e9b91fc677a3276e93b562789e46
Jinho Jo <andrewjo950515@gmail.com>
2025-10-31 23:21:12 +0900

    chore: 기본 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
@@ -9,9 +8,9 @@ if (!rootElement) {
 }
 
 ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>
+  
 );
 
 
